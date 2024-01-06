1a5906756bb217c499eeeb5edcd27e35627bb1f1
ramazansartayev <83175478+Ramazan-maker@users.noreply.github.com>
2024-01-06 23:54:06 +0600

    homework17

--- testapp/urls.py
+++ testapp/urls.py
@@ -1,7 +1,8 @@
 # testapp/urls.py
 
 from django.urls import path, re_path
-from .views import TaskListView, TaskDetailView, TaskCreateView, TaskUpdateView, TaskDeleteView, SMSListView, add_task
+from .views import TaskListView, TaskDetailView, TaskCreateView, TaskUpdateView, TaskDeleteView, SMSListView, add_task, get_all_tasks, get_task, create_task, update_task, task_get_one
+
 urlpatterns = [
     path('sms/', SMSListView.as_view(), name='sms-list'),
     path('tasks/', TaskListView.as_view(), name='task-list'),
@@ -13,5 +14,11 @@ urlpatterns = [
     re_path(r'^tasks/(?P<pk>\d+)/update/$', TaskUpdateView.as_view(), name='task-update'),
 
     path('tasks/<int:pk>/delete/', TaskDeleteView.as_view(), name='task-delete'),
+    path('tasks/', get_all_tasks, name='get_all_tasks'),
+    path('tasks/<int:task_id>/', get_task, name='get_task'),
+    path('tasks/create/', create_task, name='create_task'),
+    path('tasks/<int:task_id>/update/', update_task, name='update_task'),
+    path('tasks/get_one/<int:task_id>/', task_get_one, name='get-one-task'),
 
 ]
+
--- testapp/views.py
+++ testapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Task, SMS
@@ -45,3 +45,39 @@ def add_task(request):
         form = TaskForm()
 
     return render(request, 'add_task.html', {'form': form})
+
+def get_all_tasks(request):
+    tasks = Task.objects.all()
+    return render(request, 'task_list.html', {'tasks': tasks})
+
+def get_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    return render(request, 'task_detail.html', {'task': task})
+
+def create_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('get_all_tasks')
+    else:
+        form = TaskForm()
+
+    return render(request, 'create_task.html', {'form': form})
+
+def update_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('get_all_tasks')
+    else:
+        form = TaskForm(instance=task)
+
+    return render(request, 'update_task.html', {'form': form, 'task': task})
+
+def task_get_one(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    return render(request, 'get_one_task.html', {'task': task})
